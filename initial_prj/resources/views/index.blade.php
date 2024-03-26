753e8f105c2658827ab7e4861f551ddb4f3a8293
Hron2 <saaha1254370@gmail.com>
2024-03-26 08:46:40 +0300

    upd ReadMe

--- initial_prj/resources/views/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout.main')
-
-@section('title', 'Главная')
-
-@section('body')
-    <h1 class="text-black">Welcome to my website!</h1>
-    <p class="text-green-500">This is the content of my website.</p>
-@endsection
